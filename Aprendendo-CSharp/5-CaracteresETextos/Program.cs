6e37f4d96cd87ec40eeb130344f2631bc3f3c3bc
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-23 14:05:34 -0300

    Caracteres,Textos e atribuições de variáveis
    Alterado para trabalhar com  variáveis do tipo texto (string)
    e também com atribuições.

---
+++ Aprendendo-CSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 5 Caracteres e Textos:");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia " + 2021;
+            string cursos = 
+@" -.NET 
+- Java";
+            Console.WriteLine(cursos);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aprendendo-CSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6:");
+
+            int idade = 32;
+            int idadePedro = idade;
+            idade = 30;
+            Console.WriteLine(idadePedro);
+            Console.WriteLine(idade);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
