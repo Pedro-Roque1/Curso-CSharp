b585685110e06980e4455b174bb9295c1fef7ef3
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-23 13:36:25 -0300

    Variáveis e tipos
    Alterado para entender os tipos de variáveis aceitas pela linguagem.

---
+++ Aprendendo-CSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+            int idade;
+            idade = 32;
+            int soma = (idade + 5) * 2;
+            Console.WriteLine("Sua idade somada e multiplicada é:" + soma + " anos ");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aprendendo-CSharp/4-ConversõesEOutrosTiposNuméricos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversõesEOutrosTiposNuméricos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 4 Conversões e Outros tipos numéricos:");
+            double salario;
+            salario = 1200.50;
+            
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            Console.WriteLine("A execução terminou.Tecle enter para sair");
+
+            // O long é uma variavel de 64 bits
+            long idade;
+            idade = 13000000000000000;
+            Console.WriteLine(idade);
+            //o Short é uma variavel de 16 bits
+            short quantidadeProdutos;
+            quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+            //menor prescisão com relação ao double
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+            Console.ReadLine();
+        }
+    }
+}
