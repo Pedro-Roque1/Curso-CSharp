447fd9ab08e8ae31a36d42f985b2b8b353ac9593
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-26 09:53:27 -0300

    Ligação entre classes Cliente e Conta corrente com namespace
    Alterado para haver relacionamento entre as classes Cliente e
    Conta corrente e dessa forma permitir que seus objetos populem
    a classe referenciada, através de namespaces.

---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente Raissa = new Cliente();
+            
+            //Raissa.nome = "Raissa";
+            //Raissa.profissao = "Desenvolvedora C#";
+            //Raissa.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+            //conta.titular = new Cliente();
+            //conta.titular = Raissa;
+            if (conta.titular == null) Console.WriteLine("Referencia para o campo titular nula");
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+            //conta.titular.nome = "Raissa Damasceno";
+            //Console.WriteLine(Raissa.nome);
+            //Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
