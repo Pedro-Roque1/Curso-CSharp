49d878e2fdb36f258c3bdd883923618d201bb0d5
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-27 14:42:06 -0300

    Tratando exceções específicas
    Alterado para tratar exceções do tipo nulo ou divisão por zero
    de uma maneira geral ou específica, e passar essas exceções para
    tratamento em um método.

---
+++ ByteBank/ByteBankExceptions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+           
+            Console.ReadLine();
+
+        }
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            //Console.WriteLine(conta.Saldo);
+            try
+            {
+                return numero / divisor;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
+        }
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            
+        }
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+}
