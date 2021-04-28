b7ac7cfb778cc98f9f0983a12f4474e51323bf67
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-28 15:06:32 -0300

    Pegando valor de um argumento
    Alterado para pegar o valor de um argumento da presente na url.

---
+++ ByteBank/Strings/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get;}
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (string.IsNullOrEmpty(url)) throw new ArgumentException("O argumento url não pode ser nulo ou vazio. ", nameof(url));
+
+            URL = url;
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = URL.Substring(indiceInterrogacao + 1);
+
+        }
+        
+    }
+}
