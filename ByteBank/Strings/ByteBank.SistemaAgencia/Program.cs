54a9d3097075446ab5628236a8e188ad598bfeff
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-29 08:07:21 -0300

    Classe Object
    Alterado para mostrar conteudo de contas ou funcionários criados e
    para explorar a classe Object.

--- ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
@@ -13,36 +13,29 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            //string padrao ="[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
-            string padrao = "[0-9]{4,5}-?[0-9]{4}";
-            string textoDeTeste = "Meu nome é Guilherme, me ligue em 94784-4444";
-            //Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao)); Me retorna um bool
-            Match resultado = Regex.Match(textoDeTeste, padrao);
-            Console.WriteLine(resultado.Value);// Me retorna um valor
+            Console.WriteLine("Olá, mundo!");
+            Console.WriteLine(123);
+            Console.WriteLine(10.5);
+            Console.WriteLine(true);
+
+            object conta = new ContaCorrente(456, 687876);
+            Console.WriteLine(conta);
+            string contaToString = conta.ToString();
+            Console.WriteLine(contaToString);
+            
+            object desenvolvedor = new Desenvolvedor("4564654");
+            Console.WriteLine(desenvolvedor);
+
             Console.ReadLine();
 
-            string urlTeste = "https://www.bytebank.com/cambio";
-            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
-            
-            urlTeste.StartsWith("https://www.bytebank.com");//começa com
-            Console.WriteLine(urlTeste.EndsWith("cambio"));//termina com 
-            Console.WriteLine(urlTeste.Contains("bytebank")); // contém parametro
-
-            Console.WriteLine(indiceByteBank == 0);
-            
-            Console.ReadLine();
-            string urlPametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=2500";
-            ExtratorValorDeArgumentosURL extratorValores = new ExtratorValorDeArgumentosURL(urlPametros);
-
-            string valor = extratorValores.GetValor("moedadestino");
-            Console.WriteLine("Valor de moeda Destino:" + valor);
-            string valorMoedaOrigem = extratorValores.GetValor("moedaOrigem");
-            Console.WriteLine("Valor de Moeda Origem:" + valorMoedaOrigem);
-            Console.ReadLine();
-            
 
 
         }
+
+
+       
+
+     
     }
 
 
