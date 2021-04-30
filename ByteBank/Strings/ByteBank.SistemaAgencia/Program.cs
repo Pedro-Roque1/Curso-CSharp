0e584751ce99d28ac6af4aadaa611aa75fb1d35a
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-30 11:41:39 -0300

    Filtrando e listando contas
    Alterado para filtrar a lista de contas e não lançar exceções caso
    um item da lista tenha o valor nulo.

--- ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
@@ -20,18 +20,26 @@ namespace ByteBank.SistemaAgencia
 
             var contas = new List<ContaCorrente>()
             {
-                        new ContaCorrente(341, 57480),
-                        new ContaCorrente(342, 45678),
-                        new ContaCorrente(340, 1),
-                        new ContaCorrente(290, 18950)
-            };
+                        new ContaCorrente(341, 1),
+                        null,
+                        new ContaCorrente(342, 2),
+                        null,
+
+                        new ContaCorrente(340, 3),
+                        new ContaCorrente(290, 4)            };
 
             //contas.Sort();-> Chamar a implementação dada em IComparable
-            contas.Sort(new ComparadorContaCorrentePorAgencia());
+            //contas.Sort(new ComparadorContaCorrentePorAgencia());
 
+            
+            var ContasOrdenadas =
+                contas
+                .Where(conta => conta != null)
+                .OrderBy(conta => conta.Agencia);
 
-            foreach (var conta in contas)
+            foreach (var conta in ContasOrdenadas)
             {
+               
                 Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
             }
 
@@ -41,42 +49,8 @@ namespace ByteBank.SistemaAgencia
 
 
 
-            ///////////////testando Sort()//////////////////////
-            var nomes = new List<string>()
-            {
-                "Pedro",
-                "Guilherme",
-                "Raissa",
-                "SLA"
-            };
-            nomes.Sort();
-
-            foreach (var nome in nomes)
-            {
-                Console.WriteLine(nome);
-            }
-
-
-            var  idades = new List<int>();
-            idades.Add(1);
-            idades.Add(5);
-            idades.Add(14);
-            idades.Add(25);
-            idades.Add(38);
-            idades.Add(61);
-
-            idades.AdicionarVarios(99, -1);
-            idades.Sort();
-            //ListExtensoes.AdicionarVarios(idades, 1, 5687, 1987, 1567, 987);
-
-            idades.AdicionarVarios(5, 46, 47);
-
-            for (int i = 0;  i< idades.Count; i++)
-            {
-                Console.WriteLine(idades[i]);
-            }
-
-            Console.ReadLine();
+            
+            
         }
         
        
