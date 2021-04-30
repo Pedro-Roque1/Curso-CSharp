af4cae63627b6425e2908e34be650483fb206e5c
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-30 16:14:19 -0300

    Criado método para criar arquivo do tipo csv
    Alterado para criar arquivos do tipo csv para inserir novas contas.

--- ByteBank/ImportacaoExportacao/Program.cs
+++ ByteBank/ImportacaoExportacao/Program.cs
@@ -12,43 +12,9 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
-
-            var enderecoDoArquivo = "contas.txt";
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-                using (var leitor = new StreamReader(fluxoDeArquivo))
-                    while (!leitor.EndOfStream)
-                    {
-                         var linha = leitor.ReadLine();
-                         var contaCorrente = ConverterStringParaContaCorrente(linha);
-                         var msg = $" {contaCorrente.Titular.Nome} : Numero : {contaCorrente.Numero}, ag. {contaCorrente.Agencia},saldo: {contaCorrente.Saldo}";
-
-                         Console.WriteLine(msg);
-                    }
-
-                Console.ReadLine();
+            CriarArquivoComWriter();
+            Console.WriteLine("finalizando ... ");
+            Console.ReadLine();
         }
-        static ContaCorrente ConverterStringParaContaCorrente(string linha)
-        {
-            string [] campos = linha.Split(',');
-            string agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.',',');
-            var nomeTitular = campos[3];
-
-            var agenciaComoInt = int.Parse(agencia);
-            var numeroComoInt = int.Parse(numero);
-            var saldoComoDouble = double.Parse(saldo);
-
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
-            resultado.Depositar(saldoComoDouble);
-            resultado.Titular = titular;
-
-            return resultado;
-        }
-
-
-
     }
 }
