f881968e9f185361645965d8f092d873cf461684
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-30 15:19:54 -0300

    Abrir e ler arquivos de Texto
    Abrindo arquivos de texto,lendo linha a linha e retornando dados
    na tela.

---
+++ ByteBank/ImportacaoExportacao/LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,41 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao { 
+partial class Program
+{
+    static void LidandoComFileStramDiretamente()
+    {
+        var caminhoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(caminhoArquivo, FileMode.Open))
+        {
+            var buffer = new byte[1024]; //1KB
+            var numeroDeBytesLidos = -1;
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+
+            }
+
+        }
+         void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.Default;// Decodificação padrão do sistema operacional
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
+}
---
+++ ByteBank/ImportacaoExportacao/Program.cs
@@ -0,0 +1,37 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void Main(string[] args) 
+        {
+
+            var enderecoDoArquivo = "contas.txt";
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                
+                var leitor = new StreamReader(fluxoDeArquivo);
+               
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+                
+            }
+           
+            Console.ReadLine();
+        }
+
+        
+        
+    }
+} 
+ 
