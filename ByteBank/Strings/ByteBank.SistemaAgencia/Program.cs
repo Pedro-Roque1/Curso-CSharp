cd4660b4040306e896068b4fcfe1d13e31bb0790
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-05-03 11:30:20 -0300

    Páginas de listas de livros pasadas na url
    Alterado para mostrar listas de livros "lidos" e "lendo", as quais
    são acessadas através da Url "/Livros/Lista a ser acessada".
    Anteriormente tínhamos uma página com somente um tipo de lista.

--- ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/Strings/ByteBank.SistemaAgencia/Program.cs
@@ -18,31 +18,8 @@ namespace ByteBank.SistemaAgencia
         {
 
 
-            var contas = new List<ContaCorrente>()
-            {
-                        new ContaCorrente(341, 1),
-                        null,
-                        new ContaCorrente(342, 2),
-                        null,
 
-                        new ContaCorrente(340, 3),
-                        new ContaCorrente(290, 4)            };
-
-            //contas.Sort();-> Chamar a implementação dada em IComparable
-            //contas.Sort(new ComparadorContaCorrentePorAgencia());
-
-            
-            var ContasOrdenadas =
-                contas
-                .Where(conta => conta != null)
-                .OrderBy(conta => conta.Agencia);
-
-            foreach (var conta in ContasOrdenadas)
-            {
-               
-                Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
-            }
-
+            var enderecodoArquivo = "contas.txt";
             Console.ReadLine();
 
 
