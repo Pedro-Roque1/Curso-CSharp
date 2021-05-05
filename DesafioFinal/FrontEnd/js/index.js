999f2acd8f46cd5127ddd42661bdea0414d84c29
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-05-05 18:06:17 -0300

    Campo de busca
    Alterado para disponibilizar um campo de busca dos posts da API
    consumida e retornar somente o post com o item passado no campo
    de pesquisa.

--- DesafioFinal/FrontEnd/js/index.js
+++ DesafioFinal/FrontEnd/js/index.js
@@ -1,42 +1,25 @@
-var listaPosts = new Vue({
-  el: "#listagem",
-  data() {
-    return {
-      lista: [],
-    };
-  },
-  created() {
-    var self = this;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:5000/Posts");
-    xhr.addEventListener("load", function () {
-      var resposta = xhr.responseText;
-      self.lista = JSON.parse(resposta).slice(0, 10);
-    });
-    xhr.send();
-  }
-});
-
-
-
-
-
-var pesquisa = new Vue({
-    el: "#pesquisa",
-    data: {
-        post: [],
-        busca:''
+var app = new Vue({
+    el: "#app",
+    data() {
+        return {
+            lista: [],
+            busca: ''
+        };
     },
     created() {
-        var self = this;
-        var pesquisa = self.busca;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", `http://localhost:5000/Posts/Index/${pesquisa}`);
-        xhr.addEventListener("load", function () {
-            var resposta = xhr.responseText;
-            self.lista = JSON.parse(resposta).slice(0, 10);
-        });
-        xhr.send();
+        this.loadDados();
+    },
+    methods: {
+        loadDados: function () {
+            var self = this;
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", `http://localhost:5000/Posts?buscar=${this.busca}`);
+            xhr.addEventListener("load", function () {
+                var resposta = xhr.responseText;
+                self.lista = JSON.parse(resposta).slice(0, 10);
+            });
+            xhr.send();
+        }
     }
 });
